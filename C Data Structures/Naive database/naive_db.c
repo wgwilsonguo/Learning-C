024e4e47f6744e7e3ecd853b2f206856b61070fb
wgwilsonguo <wgwilsonguo@gmail.com>
2022-10-28 14:37:09 -0400

    Editing naive_db.c

--- C Data Structures/Naive database/naive_db.c
+++ C Data Structures/Naive database/naive_db.c
@@ -1,52 +1,64 @@
-#include "naive_db.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
+#define MAX_DATA 512
+#define MAX_ROWS 100
 
+struct Address {
+    int id;
+    int set;
+    char name[MAX_DATA];
+    char email[MAX_DATA];
+};
 
-// Helper function to print out errors:
-void die(const char *message){
+struct Database {
+    struct Address rows[MAX_ROWS];
+};
+
+struct Connection {
+    FILE *file;
+    struct Database *db;
+};
+
+void die(const char *message)
+{
     if (errno) {
         perror(message);
     } else {
         printf("ERROR: %s\n", message);
     }
+
     exit(1);
 }
 
-void print_address(Address *input_address){
-    printf("ID: %d\nSet: %d\nName: %s\nEmail: %s", input_address->id, input_address->set, input_address->name, input_address->email);
+void Address_print(struct Address *addr)
+{
+    printf("%d %s %s\n", addr->id, addr->name, addr->email);
 }
 
-void database_load(Connection *conn){
-    // size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
-    /*
-    ptr − This is the pointer to a block of memory with a minimum size of size*nmemb bytes.
-
-    size − This is the size in bytes of each element to be read.
-
-    nmemb − This is the number of elements, each one with a size of size bytes.
-
-    stream − This is the pointer to a FILE object that specifies an input stream.
-    */
-    int rc = fread(conn->db, sizeof(Connection), 1, conn->file);
+void Database_load(struct Connection *conn)
+{
+    int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
     if (rc != 1)
         die("Failed to load database.");
 }
 
-Connection *Database_open(const char *filename, char mode){
-    Connection *conn = malloc(sizeof(Connection));
+struct Connection *Database_open(const char *filename, char mode)
+{
+    struct Connection *conn = malloc(sizeof(struct Connection));
     if (!conn)
         die("Memory error");
 
-    conn->db = malloc(sizeof(Database));
-
-    if (!conn->db) //malloc failed somehow... not enough space??
+    conn->db = malloc(sizeof(struct Database));
+    if (!conn->db)
         die("Memory error");
 
     if (mode == 'c') {
         conn->file = fopen(filename, "w");
-    } 
-
-    else {
+    } else {
         conn->file = fopen(filename, "r+");
 
         if (conn->file) {
@@ -54,123 +66,93 @@ Connection *Database_open(const char *filename, char mode){
         }
     }
 
-    if (!conn->file){
+    if (!conn->file)
         die("Failed to open the file");
-    }
+
     return conn;
 }
 
-void Database_close(Connection *conn){
+void Database_close(struct Connection *conn)
+{
     if (conn) {
-        if (conn->file){
-            // close the file
+        if (conn->file)
             fclose(conn->file);
-        }
-        if (conn->db){
-            // free all the memory from the database
+        if (conn->db)
             free(conn->db);
-            // db is an array of addresses
-        }
         free(conn);
     }
 }
 
-void Database_write(Connection *conn)
+void Database_write(struct Connection *conn)
 {
     rewind(conn->file);
-    //rewind() moves the file pointer back to the beginning of the file
 
-    //write db to conn->file
-    int rc = fwrite(conn->db, sizeof(Database), 1, conn->file);
-    /*
-    size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
-
-        ptr − This is the pointer to the array of elements to be written.
-        size − This is the size in bytes of each element to be written.
-        nmemb − This is the number of elements, each one with a size of size bytes.
-        stream − This is the pointer to a FILE object that specifies an output stream.
-    */
+    int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
     if (rc != 1)
         die("Failed to write database.");
 
     rc = fflush(conn->file);
-    /*
-    Buffering works in such a way that the contents of an output buffer are only written 
-    to the stdout stream or FILE object ONCE the buffer is FULL, or there is a new line 
-    character at the end of it.
-    */
     if (rc == -1)
         die("Cannot flush database.");
 }
 
-
-void Database_create(Connection *conn){
-    // Given an initialized Connection struct, create the empty rows and add them to the 
-    // Connection struct's database
-    int i;
+void Database_create(struct Connection *conn)
+{
+    int i = 0;
 
     for (i = 0; i < MAX_ROWS; i++) {
-        // Make a bunch of rows to be added to a database:
-        Address addr = {.id = i,.set = 0 };
-        // Get the row from the database from the connection
-        conn->db->rows[i] = addr; 
+        // make a prototype to initialize it
+        struct Address addr = {.id = i,.set = 0 };
+        // then just assign it
+        conn->db->rows[i] = addr;
     }
 }
 
-
-void Database_set(Connection *conn, int id, const char *name, const char *email){
-    // Set one of the rows(row id = id) in the Connection struct's database
-    Address *addr = &conn->db->rows[id];
-
-    if (addr->set){
+void Database_set(struct Connection *conn, int id, const char *name,
+        const char *email)
+{
+    struct Address *addr = &conn->db->rows[id];
+    if (addr->set)
         die("Already set, delete it first");
-    }
 
     addr->set = 1;
-
-    // WARNING: bug: what happens when strncpy is passed n >= 512
-            // The last character is NOT going to be \0, so when we use it, it could not end where we meant to end it
-    // char *strncpy(char *dest, const char *src, size_t n)
+    // WARNING: bug, read the "How To Break It" and fix this
     char *res = strncpy(addr->name, name, MAX_DATA);
     addr->name[MAX_DATA -1] = '\0';
-    // demonstrate the strncpy bug....
-
+    // demonstrate the strncpy bug
     if (!res)
         die("Name copy failed");
 
     res = strncpy(addr->email, email, MAX_DATA);
     addr->email[MAX_DATA -1] = '\0';
-
     if (!res)
         die("Email copy failed");
 }
 
+void Database_get(struct Connection *conn, int id)
+{
+    struct Address *addr = &conn->db->rows[id];
 
-void Database_get(Connection *conn, int id){
-    // Print one of the rows in the conn->database
-    Address *addr = &conn->db->rows[id];
-    
     if (addr->set) {
         Address_print(addr);
-    } 
-    else {
+    } else {
         die("ID is not set");
     }
 }
 
-void Database_delete(Connection *conn, int id){
-    // Empty the database row at the input id
-    Address addr = {.id = id, .set = 0 };
+void Database_delete(struct Connection *conn, int id)
+{
+    struct Address addr = {.id = id,.set = 0 };
     conn->db->rows[id] = addr;
 }
 
-void Database_list(Connection *conn){
-    // Print all the "set" rows in the connection's database
+void Database_list(struct Connection *conn)
+{
     int i = 0;
-    Database *db = conn->db;
+    struct Database *db = conn->db;
 
     for (i = 0; i < MAX_ROWS; i++) {
-        Address *cur = &db->rows[i];// rows[] is an array of addresses
+        struct Address *cur = &db->rows[i];
 
         if (cur->set) {
             Address_print(cur);
@@ -178,71 +160,51 @@ void Database_list(Connection *conn){
     }
 }
 
-
-
-
-
 int main(int argc, char *argv[])
 {
-    if (argc < 3){
+    if (argc < 3)
         die("USAGE: ex17 <dbfile> <action> [action params]");
-    }
 
     char *filename = argv[1];
-    // c=create, g=get, s=set, d=del, l=list
-    char action = argv[2][0]; // First character in the third argument passed into the command line
-    Connection *conn = Database_open(filename, action);
+    char action = argv[2][0];
+    struct Connection *conn = Database_open(filename, action);
     int id = 0;
 
-    if (argc > 3){
-        id = atoi(argv[3]);
-    }
-
-    if (id >= MAX_ROWS){
-        die("There's not that many records.");
-    }
+    if (argc > 3) id = atoi(argv[3]);
+    if (id >= MAX_ROWS) die("There's not that many records.");
 
     switch (action) {
-        // Create a database
         case 'c':
             Database_create(conn);
             Database_write(conn);
             break;
 
-        // Get one of the rows from the database
         case 'g':
-            if (argc != 4){
+            if (argc != 4)
                 die("Need an id to get");
-            }
 
             Database_get(conn, id);
             break;
 
-        // Write into one of the rows in database
         case 's':
-            if (argc != 6){
+            if (argc != 6)
                 die("Need id, name, email to set");
-            }
 
             Database_set(conn, id, argv[4], argv[5]);
             Database_write(conn);
             break;
 
-        // Delete a database row 
         case 'd':
-            if (argc != 4){
+            if (argc != 4)
                 die("Need id to delete");
-            }
 
             Database_delete(conn, id);
             Database_write(conn);
             break;
 
-        // List or print the rows in the database
         case 'l':
             Database_list(conn);
             break;
-
         default:
             die("Invalid action: c=create, g=get, s=set, d=del, l=list");
     }
@@ -251,16 +213,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
